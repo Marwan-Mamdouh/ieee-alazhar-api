9fef572b1905f107e06d1520b48372ce3689f1e4
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-05-05 14:13:07 +0300

    feat: add validation on db level to keep our data clean

--- src/modules/board/model.ts
+++ src/modules/board/model.ts
@@ -1,9 +1,13 @@
-import mongoose, { Document, model, Schema } from "mongoose";
+import { Document, model, Schema } from "mongoose";
 import {
   BOARD_POSITIONS,
-  MEMBER_TYPES,
+  BOARD_TYPES,
+  OFFICERPOSITIONS,
+  TECHNICALPOSITIONS,
   type BoardPosition,
   type MemberType,
+  type OfficerPosition,
+  type TechnicalPosition,
 } from "./board.types.js";
 
 export interface BoardDocument extends Document {
@@ -21,19 +25,43 @@ export interface BoardDocument extends Document {
 const boardSchema = new Schema<BoardDocument>(
   {
     name: { type: String, required: true },
-    bio: { type: String, required: true },
+    bio: { type: String, required: false },
     image_url: { type: String, required: false },
     linkedin_url: { type: String, required: false },
-    position: { type: String, enum: BOARD_POSITIONS, required: true },
-    memberType: { type: String, enum: MEMBER_TYPES, required: true },
+    position: {
+      type: String,
+      enum: BOARD_POSITIONS,
+      required: true,
+      validate: {
+        validator: function (
+          this: any,
+          position: OfficerPosition | TechnicalPosition,
+        ) {
+          // 'this' refers to the document being saved
+          if (this.memberType === "officer") {
+            return (OFFICERPOSITIONS as ReadonlyArray<string>).includes(
+              position,
+            );
+          } else {
+            return (TECHNICALPOSITIONS as ReadonlyArray<string>).includes(
+              position,
+            );
+          }
+        },
+        message: (props: any) =>
+          `${props.value} is not a valid position for this member type!`,
+      },
+    },
+    memberType: { type: String, enum: BOARD_TYPES, required: true },
     boardYear: { type: Number, required: true },
   },
   { timestamps: true },
 );
 
 // id, name, position, image_url AS image, bio, linkedin_url AS linkedin, board_type, year
-// - position	- Enum [treasurer - secretary - head - vice]
-// - member_type	- Enum [officer -  technical - branding - operation - chair]
+// - member_type	- Enum [officer -  technical - branding - operation]
+// - position	- Enum for member type officer [treasurer - secretary - vice]
+// - position	- Enum for any other member type [head - vice]
 //
 // Index for the memberType query (since you'll be filtering by multiple types)
 boardSchema.index({ boardYear: -1, memberType: 1 });
