f720b766799fa93bc32d6f166f9ddb57f45c0c46
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-05-11 14:12:08 +0300

    feat: setup better auth lib for auth and add auth middleware and use it in board router, setup auth router in the server.ts file, add LLMs.txt for better llm read for better auth

---
+++ src/util/auth.ts
@@ -0,0 +1,22 @@
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { openAPI } from "better-auth/plugins";
+import type { mongo } from "mongoose";
+
+let auth: any;
+const getAuth = (client?: mongo.MongoClient) => {
+	if (client) {
+		auth ??= betterAuth({
+			database: mongodbAdapter(client.db("IEEE"), { client }),
+			emailAndPassword: { enabled: true },
+			rateLimit: {
+				window: 60 * 5,
+				max: 10,
+			},
+			plugins: [openAPI()],
+		});
+	}
+	return auth;
+};
+
+export default getAuth;
