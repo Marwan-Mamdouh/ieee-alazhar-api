778e554f1fc7a85edf6e7171434838d9d4b26498
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-05-12 01:04:18 +0300

    refactor: rearrange some middlewares in server.ts and remove unwanted import in board router

--- src/modules/board/board.router.ts
+++ src/modules/board/board.router.ts
@@ -1,4 +1,4 @@
-import { Router, type Response, type Request } from "express";
+import { Router, type Response } from "express";
 import { validate } from "../../middlewares/validate.js";
 import {
   addBoardMemberSchema,
