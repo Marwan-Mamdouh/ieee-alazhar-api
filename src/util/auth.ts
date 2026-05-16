8148a731b4812731a388c352cf660b40dcdb866a
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-05-17 02:41:49 +0300

    refactor: simplify auth be removing the wrapper function and call better auth directly,  enforce singleton pattern for the connection db function form db.ts, use the new auth handler in the imports

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -1,45 +1,41 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { emailOTP, openAPI } from "better-auth/plugins";
-import type { mongo } from "mongoose";
 
 import env from "../config/env.js";
 import mailService from "../modules/mail/mail.service.js";
+import connectDb from "../config/db.js";
 
-let auth: any;
-const getAuth = (client?: mongo.MongoClient) => {
-	if (client) {
-		auth ??= betterAuth({
-			database: mongodbAdapter(client.db("IEEE"), { client }),
-			emailAndPassword: { enabled: true },
-			rateLimit: {
-				window: 60 * 5,
-				max: 10,
-			},
-			advanced: {
-				disableOriginCheck: env.NODE_ENV !== "production",
-			},
-			plugins: [
-				openAPI(),
-				emailOTP({
-					overrideDefaultEmailVerification: true,
-					async sendVerificationOTP({ email, otp, type }) {
-						if (type === "sign-in") {
-							// Send the OTP for sign in
-							mailService.sendSignInOTP(email, otp).catch(console.error);
-						} else if (type === "email-verification") {
-							// Send the OTP for email verification
-							mailService.sendVerificationOTP(email, otp).catch(console.error);
-						} else {
-							// Send the OTP for password reset
-							mailService.sendResetPasswordOTP(email, otp).catch(console.error);
-						}
-					},
-				}),
-			],
-		});
-	}
-	return auth;
-};
+const { mongoClient: client } = await connectDb();
 
-export default getAuth;
+const auth = betterAuth({
+	database: mongodbAdapter(client.db("IEEE"), { client }),
+	emailAndPassword: { enabled: true },
+	rateLimit: {
+		window: 60 * 5,
+		max: 10,
+	},
+	advanced: {
+		disableOriginCheck: env.NODE_ENV !== "production",
+	},
+	plugins: [
+		openAPI(),
+		emailOTP({
+			overrideDefaultEmailVerification: true,
+			async sendVerificationOTP({ email, otp, type }) {
+				if (type === "sign-in") {
+					// Send the OTP for sign in
+					mailService.sendSignInOTP(email, otp).catch(console.error);
+				} else if (type === "email-verification") {
+					// Send the OTP for email verification
+					mailService.sendVerificationOTP(email, otp).catch(console.error);
+				} else {
+					// Send the OTP for password reset
+					mailService.sendResetPasswordOTP(email, otp).catch(console.error);
+				}
+			},
+		}),
+	],
+});
+
+export default auth;
