a5d1015e642ad365245c2eb6cf9bd2ee778401e5
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-05-18 11:24:37 +0300

    refactor: restructure cache setup files and zod config file

--- src/config/zod.config.ts
+++
@@ -1,6 +0,0 @@
-import { extendZodWithOpenApi } from "@asteasolutions/zod-to-openapi";
-import { z } from "zod";
-
-extendZodWithOpenApi(z); // must be before any schema files are imported
-
-export default z;
