4e005a222376dcb7c6744a6ec430583cceba1353
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-05-28 23:58:24 +0300

    feat: add feedback module and expose it in the apis

---
+++ src/modules/feedback/feedback.router.ts
@@ -0,0 +1,67 @@
+import { Router, type Request, type Response } from "express";
+
+import asyncHandler from "../../util/async.handler.js";
+import { isAuthenticated } from "../../middlewares/isAuthenticated.js";
+import { validate } from "../../middlewares/validate.js";
+import {
+	createFeedbackSchema,
+	feedbackIdSchema,
+	updateFeedbackStatusSchema,
+	type CreateFeedbackDTO,
+	type UpdateFeedbackStatusDTO,
+	type FeedbackId,
+} from "./feedback.schema.js";
+import type { TypedRequest } from "../../types/TypedRequest.js";
+import feedbackService from "./feedback.service.js";
+import {
+	paginationSchema,
+	type PaginationParams,
+} from "../../util/zod.config.js";
+
+const router = Router();
+
+router.post(
+	"/",
+	validate(createFeedbackSchema, "body"),
+	asyncHandler(async (req: TypedRequest<CreateFeedbackDTO>, res: Response) => {
+		await feedbackService.createFeedback(req.validatedBody!);
+		res.status(201).json({ message: "Feedback received" });
+	}),
+);
+
+router.get(
+	"/",
+	// isAuthenticated,
+	validate(paginationSchema, "query"),
+	asyncHandler(
+		async (
+			req: TypedRequest<unknown, unknown, PaginationParams>,
+			res: Response,
+		) => {
+			const result = await feedbackService.getFeedbacks(req.validatedQuery!);
+			res.json({ data: result });
+		},
+	),
+);
+
+router.patch(
+	"/:id/status",
+	// isAuthenticated,
+	validate(feedbackIdSchema, "params"),
+	validate(updateFeedbackStatusSchema, "body"),
+	asyncHandler(
+		async (
+			req: TypedRequest<UpdateFeedbackStatusDTO, FeedbackId>,
+			res: Response,
+		) => {
+			const result = await feedbackService.updateFeedbackStatus(
+				req.validatedParams!.id,
+				req.validatedBody!,
+			);
+
+			res.json({ data: result });
+		},
+	),
+);
+
+export default router;
---
+++ src/modules/feedback/feedback.types.ts
@@ -0,0 +1,23 @@
+export const FEEDBACK_STATUSES = [
+	"unread",
+	"read",
+	"archived",
+	"resolved",
+] as const;
+
+export const feedbackProps =
+	"_id name email phoneNumber message status handledAt createdAt updatedAt" as const;
+
+export interface FeedbackItem {
+	_id: string;
+	name: string;
+	email: string;
+	phoneNumber: string;
+	message: string;
+	status: FeedbackStatus;
+	handledAt?: Date;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+export type FeedbackStatus = (typeof FEEDBACK_STATUSES)[number];
