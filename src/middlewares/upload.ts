84e29c34275395432c553906ad8575c0eb755f82
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-06-01 17:55:06 +0300

    refactor: extend the allowed size to 6 MB and convert the allowed types to set instead of array

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -3,30 +3,31 @@ import { type Request } from "express";
 import multer, { type FileFilterCallback } from "multer";
 import { ValidationError } from "../errors/app.error.js";
 
-const ALLOWED_MIME_TYPES = [
-  "image/jpg",
-  "image/jpeg",
-  "image/png",
-  "image/webp",
-];
-const MAX_SIZE_MB = 5;
+const ALLOWED_MIME_TYPES = new Set<string>([
+	"image/jpg",
+	"image/jpeg",
+	"image/png",
+	"image/webp",
+]);
+
+const MAX_SIZE_MB = 6;
 
 const fileFilter = (
-  _: Request,
-  file: Express.Multer.File,
-  cb: FileFilterCallback,
+	_: Request,
+	file: Express.Multer.File,
+	cb: FileFilterCallback,
 ) => {
-  if (ALLOWED_MIME_TYPES.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(new ValidationError(`Unsupported file type: ${file.mimetype}`));
-  }
+	if (ALLOWED_MIME_TYPES.has(file.mimetype)) {
+		cb(null, true);
+	} else {
+		cb(new ValidationError(`Unsupported file type: ${file.mimetype}`));
+	}
 };
 
 const multerUpload = multer({
-  storage: multer.memoryStorage(),
-  limits: { fileSize: MAX_SIZE_MB * 1024 * 1024, files: 1 },
-  fileFilter,
+	storage: multer.memoryStorage(),
+	limits: { fileSize: MAX_SIZE_MB * 1024 * 1024, files: 1 },
+	fileFilter,
 });
 
 export default multerUpload;
