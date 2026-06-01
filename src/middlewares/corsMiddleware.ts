10a781c146ba7bbeaa99358738d197127992d27b
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-06-01 18:26:49 +0300

    refactor: change the error type in cors midlleware to ForbiddenError instead of raw error

--- src/middlewares/corsMiddleware.ts
+++ src/middlewares/corsMiddleware.ts
@@ -1,16 +1,18 @@
 import cors from "cors";
+import { ForbiddenError } from "../errors/app.error.js";
 
 const allowedOrigins = new Set([
 	"https://ieee-al-azhar-university.web.app",
 	"https://ieee-al-azhar-university.firebaseapp.com",
 	"https://ieee-alazhar-web.vercel.app/",
 	"http://localhost:5173",
+	// "http://localhost:4173",
 ]);
 
 const corsMiddleware = cors({
 	origin: (origin, cb) => {
 		if (!origin || allowedOrigins.has(origin)) cb(null, true);
-		else cb(new Error("Not allowed by CORS"));
+		else cb(new ForbiddenError("Not allowed by CORS"));
 	},
 	methods: ["GET", "POST", "PATCH", "DELETE"],
 	credentials: true,
