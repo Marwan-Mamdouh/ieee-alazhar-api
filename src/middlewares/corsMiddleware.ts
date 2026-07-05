9cfdfbccf8160c486bb68abd91ec46163595d8bb
Marwan Mamdouh AbdalMagied <marwanabdalmagied@gmail.com>
2026-07-05 05:13:52 +0300

    fix: update the allowed origins (#23)

--- src/middlewares/corsMiddleware.ts
+++ src/middlewares/corsMiddleware.ts
@@ -2,21 +2,19 @@ import cors from "cors";
 import { ForbiddenError } from "../errors/app.error.js";
 
 const allowedOrigins = new Set([
-	"https://ieee-al-azhar-university.web.app",
-	"https://ieee-al-azhar-university.firebaseapp.com",
-	"https://ieee-alazhar-web.vercel.app",
-	"http://localhost:5173",
-	// "http://localhost:4173",
+  "https://ieee-website-phi.vercel.app",
+  "http://localhost:5173",
+  // "http://localhost:4173",
 ]);
 
 const corsMiddleware = cors({
-	origin: (origin, cb) => {
-		if (!origin || allowedOrigins.has(origin)) cb(null, true);
-		else cb(new ForbiddenError("Not allowed by CORS"));
-	},
-	methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
-	credentials: true,
-	allowedHeaders: ["Content-Type", "Authorization"],
+  origin: (origin, cb) => {
+    if (!origin || allowedOrigins.has(origin)) cb(null, true);
+    else cb(new ForbiddenError("Not allowed by CORS"));
+  },
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
 });
 
 export default corsMiddleware;
